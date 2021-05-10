540fa08e2bf589b8f6c1dcddb4e45f17a91efe13
Ulman Pablo <ULMAN@kapsch.net>
2021-05-10 11:27:03 -0300

    varios

---
+++ GestionSimple.API/Helpers/AppSettingsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionSimple.API.Helpers {
+    /// <summary>
+    ///     Clase que ayuda a obtener los datos de los archivos de configuracion de la aplicacion.
+    /// </summary>
+    public class AppSettingsHelper {
+
+        /// <summary>
+        ///     Obtiene de la clave "key" del archivo de configuracion, en caso de no existir retorna "defaultValue".
+        /// </summary>
+        /// <param name="key">Clave</param>
+        /// <param name="defaultValue">Valor a devolver en caso de no existir.</param>
+        /// <returns></returns>
+        public static string GetAppSetting(string key, string defaultValue) {
+            string strReturnValue = defaultValue;
+            string strValue;
+
+            strValue = ConfigurationManager.AppSettings[key];
+            if (strValue != null) {
+                strReturnValue = strValue;
+            }
+            return strReturnValue;
+        }
+    }
+}
---
+++ GestionSimple.API/ModelsDTO/ResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using GestionSimple.Models;
+
+namespace GestionSimple.API.Models {
+    public class ResponseDTO {
+        public ResponseDTO() {
+            NewId           = 0;
+            Errores         = "";
+        }
+
+        public int NewId { get; set; }
+
+        public string Errores { get; set; }
+    }
+}
